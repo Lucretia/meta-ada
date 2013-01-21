beb27f93193d7ad764c86810200b4725c9d91282
Luke A. Guest <luke.guest@koparo.com>
2013-01-21 19:02:53 +0100

    Use the Add function from libhello.

--- recipes-test/hello/files/hello.adb
+++ recipes-test/hello/files/hello.adb
@@ -1,6 +1,9 @@
 with Ada.Text_IO; use Ada.Text_IO;
+with libHello;
 
 procedure Hello is
+   C : Integer := libHello.Add(5, 10);
 begin
    Put_Line ("Hello, Ada");
+   Put_Line ("Add(5, 10) = " & Integer'Image(C));
 end Hello;
